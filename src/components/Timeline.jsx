e02aa9537be188a1957ecc81dabb657599a7dccb
Simona Wurst <simona_wu@yahoo.de>
2024-03-24 08:25:09 +0100

    Display all properties of Work model in timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,39 +1,54 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
 import { PiTreePalmDuotone } from "react-icons/pi"
 
-
 function Timeline() {
+    const API_URL = import.meta.env.VITE_API_URL
+    const [work, setWork] = useState(null)
+
+    const getAllWork = () => {
+        axios.get(`${API_URL}/work`)
+            .then((response) => {
+                setWork(response.data)
+                console.log(response.data)
+            })
+            .catch((err) => {
+                console.log("Error getting Cafes from the API...");
+                console.log(err);
+            })
+    }
+    useEffect(() => {
+        getAllWork()
+    }, [])
+
     return (
-        <VerticalTimeline
-        lineColor={'#d4e4df'}
-        style={ Object }
-        >
-                          
-            <VerticalTimelineElement
-                className="text-2xl"
-                contentStyle={{ background: '#D4E4DF'}}
-                contentArrowStyle={{ borderRight: '7px solid #D4E4DF ' }}
-                iconStyle={{ background: '#BACDC7', color: '#ffff' }}
-                icon={<PiTreePalmDuotone />}
-            >
-                <h3 className="">Web Development Bootcamp</h3>
-                <h4 className="">Ironhack, remote</h4>
-                <p>
-                Full-time bootcamp (9 weeks) in Web Development. Focus: Javascript (ES6), React, Node.js, MongoDB, ExpressJS, HTML & CSS.                </p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="text-2xl"
-                contentStyle={{ background: '#D4E4DF'}}
-                contentArrowStyle={{ borderRight: '7px solid #D4E4DF ' }}
-                iconStyle={{ background: '#BACDC7', color: '#fff' }}
-                icon={<PiTreePalmDuotone />}
-            >
-                <h3 className="">Web Development Bootcamp</h3>
-                <h4 className="">Ironhack, remote</h4>
-                <p>
-                Full-time bootcamp (9 weeks) in Web Development. Focus: Javascript (ES6), React, Node.js, MongoDB, ExpressJS, HTML & CSS.                </p>
-            </VerticalTimelineElement>
+        <VerticalTimeline lineColor={'#d4e4df'} style={Object}>
+            {work === null
+                ? (<p>Please wait</p>)
+                : (work.map((workExperience, index) => (
+                    <VerticalTimelineElement
+                        key={index}
+                        className="text-2xl"
+                        contentStyle={{ background: '#D4E4DF' }}
+                        contentArrowStyle={{ borderRight: '7px solid #D4E4DF ' }}
+                        iconStyle={{ background: '#BACDC7', color: '#ffff' }}
+                        icon={<PiTreePalmDuotone />}
+                    >
+                        <h3 className="">{workExperience.name}</h3>
+                        <p>{workExperience.location}</p>
+                        <p>{workExperience.description}</p>
+                        <p>{workExperience.startDate}</p>
+                        <p>{workExperience.endDate}</p>
+                        <div>
+                            {workExperience.tags.map((tag, index) => (
+                                <label>{tag}</label>
+                            ))}
+                        </div>
+                    </VerticalTimelineElement>
+                ))
+                )}
         </VerticalTimeline>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage () {
     return (
         <>
         <div className="">
-        <h1 className="text-3xl">Simona Wurst</h1>
+        <h1 className="text-3xl">S. W.</h1>
         <h2>Full Stack Developer & Online Marketing</h2>
         <Timeline />
         </div>
