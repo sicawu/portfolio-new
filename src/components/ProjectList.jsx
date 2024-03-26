2574ebcdf63d863d2e90c48ab6e7c2079a6308c0
Simona Wurst <simona_wu@yahoo.de>
2024-03-26 10:42:26 +0100

    Fix ProjectList, add Icons

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
+import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { FaGithub } from "react-icons/fa"
-import { Link } from "react-router-dom"
+import { TbWorldWww } from "react-icons/tb"
 
 function ProjectList() {
     const API_URL = import.meta.env.VITE_API_URL
@@ -23,41 +24,40 @@ function ProjectList() {
     }, [])
 
     return (
-        
         <>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-10">
-        {project === null
-                ? (<p>Please wait</p>)
-                : (project.map((project, index) => (
-                    <div key={index}
-                    className="bg-beige p-5">
-                        <div className="mb-5">
-                            <h2>{project.name}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-10">
+                {project === null
+                    ? (<p>Please wait</p>)
+                    : (project.map((project, index) => (
+                        <div key={index}
+                            className="bg-beige p-5">
+                            <div className="mb-5">
+                                <h2>{project.name}</h2>
+                            </div>
+                            <div>
+                                <p>{project.description}</p>
+                            </div>
+                            <div className="my-5">
+                                {project.language.map((language, index) => (
+                                    <label
+                                        key={index}
+                                        className="bg-darkmint text-white mx-1 my-1 rounded text-sm"
+                                    >{language}
+                                    </label>
+                                ))}
+                            </div>
+                            <div className="flex justify-center gap-10">
+                            <Link to={`${project.deployedUrl}`}>
+                                    <TbWorldWww />
+                                </Link>
+                                <Link to={`${project.gitUrl}`}>
+                                    <FaGithub />
+                                </Link>
+                            </div>
                         </div>
-                        <div>
-                            <p>{project.description}</p>
-                        </div>
-                        <div className="my-5">
-                            {project.language.map((language, index) => (
-                                <label
-                                    key={index}
-                                    className="bg-darkmint text-white mx-1 my-1 rounded text-sm"
-                                >{language}
-                                </label>
-                            ))}
-                        </div>
-                        <div className="flex justify-center gap-10">
-                            <Link to={`${project.gitUrl}`}>
-                                <FaGithub />
-                            </Link>
-                            <Link to={`${project.depolyedUrl}`}>
-                                <FaGithub />
-                            </Link>
-                        </div>
-                    </div>
-                )))
-            }
-        </div>
+                    )))
+                }
+            </div>
         </>
     )
 }
