6c8fd01b9ed33d1f4a815b72a3e2bd1418c70260
Simona Wurst <simona_wu@yahoo.de>
2024-04-08 18:07:08 +0200

    Optimize design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,19 +7,19 @@ function HomePage() {
                 <div>
                     <h1 className="my-10 mx-3 text-5xl text-beige  font-gilda bold">Simona Wurst</h1>
                     <div className="flex flex-wrap justify-center">
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow hover:bg-lightmint">
+                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow">
                             <h2 className="text-beige">Full Stack Developer </h2>
                         </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow hover:bg-lightmint">
+                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow">
                             <h2 className="text-beige">Online Marketing Manager </h2>
                         </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow hover:bg-lightmint">
+                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow">
                             <h2 className="text-beige">SEO Specialist </h2>
                         </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow hover:bg-lightmint">
+                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow">
                             <h2 className="text-beige">Digital Nomad</h2>
                         </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow hover:bg-lightmint">
+                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded text-sm shadow">
                             <h2 className="text-beige">Creative Soul</h2>
                         </div>
                     </div>
