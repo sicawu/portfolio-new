a703bc452310de17cefc58f32a460a0b6db49fe8
Simona Wurst <simona_wu@yahoo.de>
2024-04-09 16:31:07 +0200

    Optimize code HomePage & fix font import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,18 @@
 import Timeline from "../../src/components/Timeline"
 
 function HomePage() {
+    const labels = ["Full Stack Developer", "Online Marketing Manager", "SEO Specialist", "Digital Nomad", "Creative Soul"]
     return (
         <>
             <div className="hero h-screen flex justify-center items-end overflow-hidden">
                 <div>
                     <h1 className="my-10 mx-3 text-5xl text-beige  font-gilda bold">Simona Wurst</h1>
                     <div className="flex flex-wrap justify-center">
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded shadow">
-                            <h2 className="text-beige font-gilda">Full Stack Developer </h2>
-                        </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded shadow">
-                            <h2 className="text-beige font-gilda">Online Marketing Manager </h2>
-                        </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded shadow">
-                            <h2 className="text-beige font-gilda">SEO Specialist </h2>
-                        </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded shadow">
-                            <h2 className="text-beige font-gilda">Digital Nomad</h2>
-                        </div>
-                        <div className="px-3 py-1 bg-green text-white mx-1 my-1 rounded shadow">
-                            <h2 className="text-beige font-gilda">Creative Soul</h2>
-                        </div>
+                        {labels.map((label, index) =>
+                            <div key={index} className="px-3 py-2 bg-green text-white mx-1 my-1 rounded shadow">
+                                <h2 className="text-beige font-gilda">{label}</h2>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
